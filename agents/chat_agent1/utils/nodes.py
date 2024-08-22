696b2fb6a888c53f8a502c5199626aaa3541d209
erikbohne <eriknbohne@gmail.com>
2024-08-22 11:29:59 +0200

    working with langgraph ui

--- agents/chat_agent1/utils/nodes.py
+++ agents/chat_agent1/utils/nodes.py
@@ -1,7 +1,18 @@
 from agents.chat_agent1.utils.state import GraphState
 from langchain_openai import AzureChatOpenAI
+from dotenv import load_dotenv
+import os
 
-llm = AzureChatOpenAI()
+load_dotenv()
+
+
+llm = AzureChatOpenAI(
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        model=os.getenv("AZURE_OPENAI_MODEL"),
+        api_version=os.getenv("AZURE_OPENAI_VERSION"),
+        temperature=0
+    )
 
 
 def chatbot(state: GraphState):
